30ae957bbc21d0936037e7ed31cc3a6f21b1d52e
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-03-20 23:40:21 +0100

    mui added

--- src/List.jsx
+++ src/List.jsx
@@ -1,10 +1,31 @@
 import React, { useState } from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
 
 const List = ({ countries }) => {
   const [selectedCountry, setSelectedCountry] = useState(null);
 
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   const handleCountryClick = (country) => {
     setSelectedCountry(country);
+    handleOpen();
+  };
+
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
   };
 
   return (
@@ -25,16 +46,37 @@ const List = ({ countries }) => {
         );
       })}
 
-      {selectedCountry && (
-        <div className="country-details">
-          <h2>{selectedCountry.name}</h2>
-          <p>Capital: {selectedCountry.capital}</p>
-          <p>Population: {selectedCountry.population}</p>
-          {/* diğer özellikler */}
-        </div>
-      )}
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          {selectedCountry && (
+            <>
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                {selectedCountry.name}
+              </Typography>
+              <div
+                className="flag"
+                style={{
+                  backgroundImage: `url(${selectedCountry.flag})`,
+                  width: "100px",
+                  height: "70px",
+                }}
+              ></div>
+              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                Population: {selectedCountry.population}
+              </Typography>
+              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                Capital: {selectedCountry.capital}
+              </Typography>
+            </>
+          )}
+        </Box>
+      </Modal>
     </div>
   );
 };
-
 export default List;
