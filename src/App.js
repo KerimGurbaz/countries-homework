c6cd888d79c3bfe2f9d3c003e959effd5607fccc
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-03-20 23:15:50 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -5,16 +5,14 @@ import './App.css';
 import List from "./List";
 
 function App() {
-const [countries, setCountries] = useState(data)
 
+const [countries, setCountries] = useState(data)
 
   return (
     <div className="App">
       <div className="container">
         <h1>Countries</h1>
         <List countries={countries}/>
-
-        {console.log(countries[0])}
 
       </div>
     </div>
--- src/List.jsx
+++ src/List.jsx
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 const List = ({ countries }) => {
+  const [selectedCountry, setSelectedCountry] = useState(null);
+
+  const handleCountryClick = (country) => {
+    setSelectedCountry(country);
+  };
+
   return (
     <div className="countries">
       {countries.map((country) => {
         return (
-          <div className="country">
+          <div
+            key={country.name}
+            className="country"
+            onClick={() => handleCountryClick(country)}
+          >
             <h2>{country.name}</h2>
+            <div
+              className="flag"
+              style={{ backgroundImage: `url(${country.flag})` }}
+            ></div>
           </div>
         );
       })}
+
+      {selectedCountry && (
+        <div className="country-details">
+          <h2>{selectedCountry.name}</h2>
+          <p>Capital: {selectedCountry.capital}</p>
+          <p>Population: {selectedCountry.population}</p>
+          {/* diğer özellikler */}
+        </div>
+      )}
     </div>
   );
 };
