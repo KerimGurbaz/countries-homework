3ae68120ab90d205566975840369531ecec0708b
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-03-22 23:37:22 +0100

    search bar added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import React, {useState} from "react";
-
+import React, { useState } from "react";
 import data from "./data/countries_data";
-import './App.css';
+import "./App.css";
 import List from "./List";
+import Search from "./Search";
 
 function App() {
+  const [countries, setCountries] = useState(data);
 
-const [countries, setCountries] = useState(data)
+  const handleSearch = (searchTerm) => {
+    const filteredCountries = data.filter((country) =>
+      country.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setCountries(filteredCountries);
+  };
 
   return (
     <div className="App">
       <div className="container">
-         <h1>Countries</h1>
-        <List countries={countries}/>
-
+        <h1>Countries</h1>
+        <Search onSearch={handleSearch} />
+        <List countries={countries} />
       </div>
     </div>
   );
